b6515bb2e0332de1dc7a26ef5db12b266124de1a
ramalloc <roshangupta1887@gmail.com>
2024-04-10 18:04:11 +0530

    Debugged the getChannelProfile Controller in projection

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -427,7 +427,7 @@ const getChannelProfile = asyncHandler(async (req, res) => {
 
     console.log(channel);
     if(!channel?.length){
-        throw new ApiError(400, "Channel doesn't exist...!")
+        throw new ApiError(404, "Channel doesn't exist...!")
     }
 
     return res
